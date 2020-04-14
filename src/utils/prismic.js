692c807fced2373e593d81ce6d723154d5dcbb0b
theCubix <timon.forrer@gmail.com>
2020-04-14 16:08:24 +0200

    Query by all languages

--- src/utils/prismic.js
+++ src/utils/prismic.js
@@ -2,7 +2,7 @@ const Prismic = require('prismic-javascript');
 
 const prismicQuery = async (documentType) => {
   let api = await Prismic.getApi('https://voltagearc.prismic.io/api/v2');
-  return (await api.query(Prismic.Predicates.at('document.type', documentType))).results;
+  return (await api.query(Prismic.Predicates.at('document.type', documentType), { lang: '*' })).results;
 };
 
 module.exports = { prismicQuery };
