98ca0a8bae2aee3316ec49e79b3ef8e3924ab6ed
theCubix <timon.forrer@gmail.com>
2020-04-14 14:24:26 +0200

    Cleaned up data file for homes

--- src/_data/allHomes.js
+++ src/_data/allHomes.js
@@ -1,39 +1,32 @@
-var InMemoryCache = require('apollo-cache-inmemory').InMemoryCache;
-var ApolloClient = require('apollo-client').ApolloClient;
-var gql = require('graphql-tag');
-var PrismicLink = require('apollo-link-prismic').PrismicLink;
+const { fetchByQuery } = require('../utils/prismic.js')
 
-const client = new ApolloClient({
-  link: PrismicLink({
-    uri: 'https://voltagearc.prismic.io/graphql'
-  }),
-  cache: new InMemoryCache()
-});
+const query = `{
+  allHomes {
+    edges {
+      node {
+        _meta {
+          lang
+          uid
+        }
+        hero_title
+        hero_introduction
+        hero_image
+      }
+    }
+  }
+}`
 
 const getAllHomes = async () => {
-  const result = await client.query({
-    query: gql`
-      query {
-        allHomes {
-          edges {
-            node {
-              _meta {
-                lang
-                uid
-              }
-              hero_title
-            }
-          }
-        }
-      }`
-  })
+  const result = await fetchByQuery(query);
 
   const allHomesFormatted = result.data.allHomes.edges.map((item) => {
     let data = item.node
     return {
       id: data._meta.uid,
       lang: data._meta.lang,
-      title: data.hero_title
+      title: data.hero_title,
+      intro_text: data.hero_introduction,
+      image: data.hero_image
     }
   })
 
