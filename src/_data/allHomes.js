38756ca5d417f5ceee646c17a083716d8575bfc3
theCubix <timon.forrer@gmail.com>
2020-04-14 14:09:37 +0200

    Added draft of fetch

--- src/_data/allHomes.js
+++ src/_data/allHomes.js
@@ -1,10 +1,43 @@
-module.exports = [
- {
-   lang: "en-gb",
-   title: "English"
- },
- {
-   lang: "de-ch",
-   title: "German"
- }
-]
+var InMemoryCache = require('apollo-cache-inmemory').InMemoryCache;
+var ApolloClient = require('apollo-client').ApolloClient;
+var gql = require('graphql-tag');
+var PrismicLink = require('apollo-link-prismic').PrismicLink;
+
+const client = new ApolloClient({
+  link: PrismicLink({
+    uri: 'https://voltagearc.prismic.io/graphql'
+  }),
+  cache: new InMemoryCache()
+});
+
+const getAllHomes = async () => {
+  const result = await client.query({
+    query: gql`
+      query {
+        allHomes {
+          edges {
+            node {
+              _meta {
+                lang
+                uid
+              }
+              hero_title
+            }
+          }
+        }
+      }`
+  })
+
+  const allHomesFormatted = result.data.allHomes.edges.map((item) => {
+    let data = item.node
+    return {
+      id: data._meta.uid,
+      lang: data._meta.lang,
+      title: data.hero_title
+    }
+  })
+
+  return allHomesFormatted
+}
+
+module.exports = getAllHomes();
