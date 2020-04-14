7b1c0709ac30bb107b92229fa2bb98c1216a8330
theCubix <timon.forrer@gmail.com>
2020-04-14 15:58:16 +0200

    Updated fetch for home page

--- src/_data/allHomes.js
+++ src/_data/allHomes.js
@@ -1,36 +1,8 @@
-const { fetchByQuery } = require('../utils/prismic.js')
-
-const query = `{
-  allHomes {
-    edges {
-      node {
-        _meta {
-          lang
-          uid
-        }
-        hero_title
-        hero_introduction
-        hero_image
-      }
-    }
-  }
-}`
+const { prismicQuery } = require('../utils/prismic.js');
 
 const getAllHomes = async () => {
-  const result = await fetchByQuery(query);
-
-  const allHomesFormatted = result.data.allHomes.edges.map((item) => {
-    let data = item.node
-    return {
-      id: data._meta.uid,
-      lang: data._meta.lang,
-      title: data.hero_title,
-      intro_text: data.hero_introduction,
-      image: data.hero_image
-    }
-  })
-
-  return allHomesFormatted
+  let allHomes = await prismicQuery('home');
+  return allHomes
 }
 
-module.exports = getAllHomes();
+module.exports = getAllHomes;
