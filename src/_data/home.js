943df6dfd3cc58acf1f3b63a5e2e5dfe4fdd9dfd
theCubix <timon.forrer@gmail.com>
2020-04-15 08:10:52 +0200

    Normalised object presentation

--- src/_data/home.js
+++ src/_data/home.js
@@ -3,8 +3,8 @@ const { prismicQuery } = require('../utils/prismic.js');
 const getHome = async () => {
   const allHomes = await prismicQuery('home');
   return {
-    DE: allHomes.filter((item) => item.lang === "de-ch")[0],
-    EN: allHomes.filter((item) => item.lang === "en-gb")[0]
+    de: allHomes.filter((item) => item.lang === "de-ch")[0],
+    en: allHomes.filter((item) => item.lang === "en-gb")[0]
   }
 }
 
