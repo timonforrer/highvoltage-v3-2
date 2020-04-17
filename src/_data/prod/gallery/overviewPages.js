81041a95c16ea6cda0e73558bb39788745fbb738
theCubix <timon.forrer@gmail.com>
2020-04-17 18:55:46 +0200

    Added data for gallery pages

---
+++ src/_data/prod/gallery/overviewPages.js
@@ -0,0 +1,8 @@
+const fetchData = require('../../../utils/prismic.js');
+const saveSeed = require('../../../utils/saveSeed.js');
+
+module.exports = async () => {
+  const response = await fetchData('gallery');
+  saveSeed(response, `${__dirname}/../../dev/gallery/overviewPages.json`);
+  return response;
+}
