bd263dd7323a7b0caa75a2250ebb31a14b8d3761
theCubix <timon.forrer@gmail.com>
2020-04-17 18:43:41 +0200

    Optimised function to remove redundant code

--- src/utils/saveSeed.js
+++ src/utils/saveSeed.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 module.exports = (data, path) => {
+  data = JSON.stringify(data);
   if(process.env.ELEVENTY_ENV == 'seed') {
     fs.writeFile(path, data, err => {
       if(err) {
