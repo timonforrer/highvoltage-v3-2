e5ae1123dbe3cd723bc5f9ae01769e9c8a9ca8b2
theCubix <timon.forrer@gmail.com>
2020-04-17 18:46:51 +0200

    Normalised function export

--- src/utils/prismic.js
+++ src/utils/prismic.js
@@ -1,8 +1,6 @@
 const Prismic = require('prismic-javascript');
 
-const prismicQuery = async (documentType) => {
+module.exports = async (documentType) => {
   let api = await Prismic.getApi('https://voltagearc.prismic.io/api/v2');
   return (await api.query(Prismic.Predicates.at('document.type', documentType), { lang: '*' })).results;
 };
-
-module.exports = { prismicQuery };
