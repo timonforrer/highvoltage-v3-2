3f48766cf990f7c712317647f9a0fcc0681cb2b8
timonforrer <timon.forrer@gmail.com>
2020-04-24 08:15:02 +0200

    Added logic to handle precss

---
+++ src/css/styles.11ty.js
@@ -0,0 +1,33 @@
+// Code used from Phil Hawksworths 'EleventyOne' Starter Template
+
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+
+// the file name as an entry point for postcss compilation
+// also used to define the output filename in our output /css folder.
+const fileName = "main.css";
+
+module.exports = class {
+  async data () {
+    const rawFilepath = path.join(__dirname, `../scss/${fileName}`);
+    return {
+      permalink: `css/${fileName}`,
+      rawFilepath,
+      rawCss: await fs.readFileSync(rawFilepath)
+    };
+  };
+
+  async render ({ rawCss, rawFilepath }) {
+    return await postcss([
+      // require('postcss-comment'),
+      require('precss'),
+      require('postcss-import'),
+      require('postcss-mixins'),
+      require('postcss-color-mix'),
+      require('cssnano')
+    ])
+    .process(rawCss, { from: rawFilepath })
+    .then(result => result.css);
+  };
+}
