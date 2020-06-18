c78d00cb0c7755a2445d2b8207dcd7f0f1b261bf
timonforrer <timon.forrer@gmail.com>
2020-06-18 23:23:53 +0200

    Rollup config to load webcomponents and creating bundle

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/js/index.js',
+  output: {
+    file: 'dist/assets/bundle.js',
+    format: 'es'
+  },
+  plugins: [
+    resolve(),
+    terser()
+  ]
+};
