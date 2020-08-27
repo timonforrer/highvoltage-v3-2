c4ffc8908c6dcfbdb88602d2859c5846ee2fe3ec
timonforrer <timon.forrer@gmail.com>
2020-08-27 23:56:22 +0200

    Sorting music by release date (descending)

--- src/_data/prod/music/subPagesPrismic.js
+++ src/_data/prod/music/subPagesPrismic.js
@@ -3,6 +3,11 @@ const saveSeed = require('../../../utils/saveSeed.js');
 
 module.exports = async () => {
   const response = await fetchData('music');
-  saveSeed(response, `${__dirname}/../../dev/music/subPagesPrismic.json`);
-  return response;
+  const returnArraySorted = response.sort((a, b) => {
+    a = new Date(a.data.release_date);
+    b = new Date(b.data.release_date);
+    return a>b ? -1 : a<b ? 1 : 0;
+  });
+  saveSeed(returnArraySorted, `${__dirname}/../../dev/music/subPagesPrismic.json`);
+  return returnArraySorted;
 };
