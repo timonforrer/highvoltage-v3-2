51cfbeadbf1e5946edc5f5fffd60c6e5416d8cd9
timonforrer <timon.forrer@gmail.com>
2020-11-14 02:02:19 +0100

    Fetching content for pages

---
+++ src/_data/prod/shop/subPagesAirtable.js
@@ -0,0 +1,32 @@
+const base = require('../../../utils/airtableShop.js');
+const fetchData = require('../../../utils/airtableDataController.js');
+const saveSeed = require('../../../utils/saveSeed.js');
+
+module.exports = async () => {
+  const response = await fetchData(base('Products'), 'Default', '');
+
+  const returnArray = [];
+
+  const constructBaseFields = (data) => {
+    return {
+      productNumber: data['Product Number'],
+      basePrice: data['Base Price'],
+      prismicUID: data['prismicUID']
+    };
+  };
+
+  const responseFormattedDE = response.map(item => {
+    return { ...constructBaseFields(item.fields), lang: 'de-ch' };
+  });
+
+  const responseFormattedEN = response.map(item => {
+    return { ...constructBaseFields(item.fields), lang: 'en-gb' };
+  });
+
+  responseFormattedDE.map(item => returnArray.push(item));
+  responseFormattedEN.map(item => returnArray.push(item));
+
+  saveSeed(returnArray, `${__dirname}/../../dev/shop/subPagesAirtable.json`);
+
+  return returnArray;
+}
