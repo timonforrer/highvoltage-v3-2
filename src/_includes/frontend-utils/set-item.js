923ae90a02d64325ab32dab4133999797aa2a24f
timonforrer <timon.forrer@gmail.com>
2020-12-11 17:47:52 +0100

    Switched to custom event and added polyfill

--- src/_includes/frontend-utils/set-item.js
+++ src/_includes/frontend-utils/set-item.js
@@ -1,4 +1,19 @@
-const event = new Event('itemInserted')
+// trying to polyfill event
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: null };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  window.CustomEvent = CustomEvent;
+})();
+
+const event = new CustomEvent('itemInserted')
 
 module.exports = (key,value) => {
   localStorage.setItem(key,value);
